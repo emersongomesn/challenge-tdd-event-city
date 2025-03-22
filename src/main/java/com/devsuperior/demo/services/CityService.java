904d6109b70b09e9f2df04d2c7808cbcb2cd5a7f
Emerson Gomes Nunes <emerson100loko@hotmail.com>
2025-03-22 13:54:42 -0300

    Controller and service

---
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -0,0 +1,23 @@
+package com.devsuperior.demo.services;
+
+import java.util.List;
+
+import com.devsuperior.demo.dto.CityDTO;
+
+public class CityService {
+
+	public List<CityDTO> findAll() {
+		
+		return null;
+	}
+
+	public CityDTO insert(CityDTO dto) {
+	
+		return null;
+	}
+
+	public void delete(Long id) {
+	
+	}
+
+}
